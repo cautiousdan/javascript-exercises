f3b8df734085c05479d1fa89f21a0e1c1630ea0b
cautiousdan <100444148+cautiousdan@users.noreply.github.com>
2023-03-08 19:51:23 +0000

    Completed calculator exercise.
    Added tests to spec.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let newSum = 0;
+  a.forEach (x => {
+    newSum += x;
+  })
+  return newSum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let newProduct = 1; 
+  a.forEach (x => {
+    newProduct = newProduct * x;
+  })
+  return newProduct;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = a;
+	if (a === 0 || a === 1)
+    return 1;
+  while (a > 1) {
+    a--;
+    result *= a;
+  }
+  return result
 };
 
 // Do not edit below this line
