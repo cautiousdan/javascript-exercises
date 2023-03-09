a90f1dae0fd8664e88cdb0c46588970af4928709
cautiousdan <100444148+cautiousdan@users.noreply.github.com>
2023-03-09 17:37:23 +0000

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
-
+const fibonacci = function(targetNum) {
+    if (typeof targetNum === 'string') {
+        targetNum = parseInt(targetNum, 10)
+    }
+    if (targetNum < 1) {
+        return "OOPS"
+    }
+    if (targetNum === 1) {
+        return 1
+    }
+    let currentNum = 1
+    let numberA = 0
+    let numberB = 1
+    let newNumberA = 0
+    let newNumberB = 0
+    let newSum = 0
+    while (currentNum < targetNum) {
+        newSum = numberA + numberB
+        newNumberB = newSum
+        newNumberA = numberB
+        numberB = newNumberB
+        numberA = newNumberA
+        currentNum++
+    }
+    return newSum
 };
 
 // Do not edit below this line
