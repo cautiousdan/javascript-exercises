57f724420b768cb16d26de5f4e3913be9aadf68a
cautiousdan <100444148+cautiousdan@users.noreply.github.com>
2023-03-09 16:08:23 +0000

    Finish palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    let inboundString = a;
+    inboundString = inboundString.toLowerCase();
+    inboundString = inboundString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    inboundString = inboundString.replace(/\s/g, '');
+    if (inboundString == inboundString.split('').reverse().join('')) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
